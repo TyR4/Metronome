fe8de82abe5edd68f5bda83e1bd6eea721659759
Tyler <ty.r.dave@gmail.com>
2018-02-21 20:10:48 -0700

    basic main for testing

--- Metronome/main.cpp
+++ Metronome/main.cpp
@@ -1,7 +1,23 @@
+#include <iostream>
 #include "metronome.h"
 
+using namespace std; 
+
 int main() {
-	initialTest();
+	Metronome metronome;
+
+	//cout << "Enter speed: ";
+
+	//int bpm;
+	//cin >> bpm;
+	//while (bpm < 60) {
+	//	cout << "Do not enter a tempo below 60, that's just cruel" << endl;
+	//	cin >> bpm;
+	//}
+
+
+	//metronome.run(bpm);
+	metronome.warmupOpenCloseOpen();
 
 	return 0;
 }
