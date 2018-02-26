f0b48222b0452c0fb254dd2c04eba6ef59202928
Tyler <ty.r.dave@gmail.com>
2018-02-25 17:21:31 -0700

    change to automatically start metronome at 60 BPM

--- Metronome/main.cpp
+++ Metronome/main.cpp
@@ -6,18 +6,8 @@ using namespace std;
 int main() {
 	Metronome metronome;
 
-	//cout << "Enter speed: ";
-
-	//int bpm;
-	//cin >> bpm;
-	//while (bpm < 60) {
-	//	cout << "Do not enter a tempo below 60, that's just cruel" << endl;
-	//	cin >> bpm;
-	//}
-
-
-	//metronome.run(bpm);
-	metronome.warmupOpenCloseOpen();
+	metronome.run();
+	//metronome.warmupOpenCloseOpen();
 
 	return 0;
 }
